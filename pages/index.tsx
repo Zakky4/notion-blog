67d6f096e75ce4c608f2d7bc4024b3ae0a2d9ab7
zakky4 <sadaoza@gmail.com>
2023-06-27 13:47:49 +0900

    もっと見る専用ページのレイアウトを変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,6 +43,7 @@ export default function Home({ fourPosts }) {
               date={post.date}
               tags={post.tags}
               slug={post.slug}
+              isPaginationPage={false}
             />
           </div>
         ))}
